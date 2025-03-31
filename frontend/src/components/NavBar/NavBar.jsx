2bc646e394cfdb33a7154270752a75aea70c0771
Espadv69 <aitorjob69@gmail.com>
2025-03-31 18:52:08 +0200

    Enhance NavBar component with improved styling and class names

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom'
 import { TASK_FORM_ROUTE, TASK_LIST_ROUTE } from '../../utils/routesString'
 
+import './NavBar.css'
+
 const NavBar = () => {
   return (
     <nav className="nav">
-      <h2>To-Do manager</h2>
+      <h2 className="nav__title">To-Do manager</h2>
       <div className="nav__links">
-        <Link to={TASK_LIST_ROUTE}>Tasks List</Link>
-        <Link to={TASK_FORM_ROUTE}>Task Form</Link>
+        <Link className="nav__link" to={TASK_LIST_ROUTE}>
+          Tasks List
+        </Link>
+        <Link className="nav__link" to={TASK_FORM_ROUTE}>
+          Task Form
+        </Link>
       </div>
     </nav>
   )
