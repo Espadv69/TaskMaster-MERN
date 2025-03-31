ad1e5a395ddac64a9facf215b16047af2946ad39
Espadv69 <aitorjob69@gmail.com>
2025-03-31 12:56:28 +0200

    Set up Express middleware for CORS and JSON parsing

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
