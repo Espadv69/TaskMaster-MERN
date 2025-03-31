030f4b7fb70cffa82abfdb3c7e63c05a072e5306
Espadv69 <aitorjob69@gmail.com>
2025-03-31 18:47:27 +0200

    Add NavBar component to App for improved navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { TASK_LIST_ROUTE, TASK_FORM_ROUTE } from './utils/routesString.js'
 
+import NavBar from './components/NavBar/NavBar.jsx'
 import TaskFormPage from './pages/TaskFormPage'
 import TaskListPage from './pages/TaskListPage'
 
 const App = () => {
   return (
     <Router>
-      {/* Add NavBar */}
+      <NavBar />
       <Routes>
         <Route path={TASK_LIST_ROUTE} element={<TaskListPage />} />
         <Route path={TASK_FORM_ROUTE} element={<TaskFormPage />} />
