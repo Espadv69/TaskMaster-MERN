cabe45b50ac8de2382d374bfbdcb6ef0a25cc117
Espadv69 <aitorjob69@gmail.com>
2025-03-31 17:21:16 +0200

    Add getTasks function to retrieve all tasks from the database

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,1 +1,12 @@
 import TASK_MODEL from '../models/Task.js'
+
+// Get all tasks
+export const getTasks = async (req, res) => {
+  try {
+    // Fetch all tasks from the database
+    const tasks = await TASK_MODEL.find()
+
+    // Send the tasks as a response
+    res.status(200).json(tasks)
+  } catch (err) {}
+}
