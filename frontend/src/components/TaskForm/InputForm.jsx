623cdc5894c00b39cff92bc31df9a4824a48803f
Espadv69 <aitorjob69@gmail.com>
2025-04-01 16:43:07 +0200

    Enhance InputForm component with props for label, value, and placeholder

--- frontend/src/components/TaskForm/InputForm.jsx
+++ frontend/src/components/TaskForm/InputForm.jsx
@@ -1,3 +1,23 @@
-const InputForm = () => {}
+const InputForm = ({
+  labelTitle,
+  inputValue,
+  setInputValue,
+  setInputLength,
+  placeholder,
+}) => {
+  return (
+    <label className="task-form__label">
+      {labelTitle}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="task-form__input"
+        maxLength={setInputLength}
+        placeholder={placeholder}
+      />
+    </label>
+  )
+}
 
 export default InputForm
