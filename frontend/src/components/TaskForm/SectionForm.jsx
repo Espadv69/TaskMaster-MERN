2e21d8054d17f80481eb4734f0657678a132cc32
Espadv69 <aitorjob69@gmail.com>
2025-04-01 16:46:59 +0200

    Refactor SectionForm component to accept props for label, value, and state management

--- frontend/src/components/TaskForm/SectionForm.jsx
+++ frontend/src/components/TaskForm/SectionForm.jsx
@@ -1,3 +1,21 @@
-const SectionForm = () => {}
+const SectionForm = ({
+  labelName,
+  sectionValue,
+  setSectionValue,
+  children,
+}) => {
+  return (
+    <label className="task-form__label">
+      {labelName}
+      <select
+        className="task-form__select"
+        value={sectionValue}
+        onChange={(e) => setSectionValue(e.target.value)}
+      >
+        {children}
+      </select>
+    </label>
+  )
+}
 
 export default SectionForm
